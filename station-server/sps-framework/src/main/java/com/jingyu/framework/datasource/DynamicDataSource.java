7aed603b50fd494260b7adf24da434d3ee0f8f93
jikang <15634158871@163.com>
2023-06-21 09:13:06 +0800

    将Ruoyi字段和一些文本描述修改。

--- station-server/sps-framework/src/main/java/com/jingyu/framework/datasource/DynamicDataSource.java
+++ station-server/sps-framework/src/main/java/com/jingyu/framework/datasource/DynamicDataSource.java
@@ -2,6 +2,8 @@ package com.jingyu.framework.datasource;
 
 import java.util.Map;
 import javax.sql.DataSource;
+
+import com.jingyu.common.utils.SecurityUtils;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 /**
@@ -22,5 +24,9 @@ public class DynamicDataSource extends AbstractRoutingDataSource
     protected Object determineCurrentLookupKey()
     {
         return DynamicDataSourceContextHolder.getDataSourceType();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SecurityUtils.encryptPassword("admin123"));
     }
 }
